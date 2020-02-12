9f5c09695f164e93bb8d10024c27a0cdae7f8a1e
semh <sem.huits@hva.nl>
2020-02-12 14:45:41 +0100

    hoipiepelepooi

--- Automated programming 2/src/com/company/Main.java
+++ Automated programming 2/src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        sadfasdfsaf
+
+                sadf
+
+                sdaf
+                        sda
+                        f
+                                sad
+                                f
     }
 }
