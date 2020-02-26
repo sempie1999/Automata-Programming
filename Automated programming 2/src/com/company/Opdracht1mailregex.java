aea1a8e133e2d398bc0b555c0722198eb8148fea
semh <sem.huits@hva.nl>
2020-02-26 12:43:31 +0100

    kaas

--- Automated programming 2/src/com/company/Opdracht1mailregex.java
+++ Automated programming 2/src/com/company/Opdracht1mailregex.java
@@ -1,16 +1,9 @@
 package com.company;
+import Scanner
 
 public class Opdracht1mailregex {
 
     public static void main(String[] args) {
-	// write your code here
 
-        //asdfasdf
-        ///asdfsadfj
-        //asdfasdfasdfsdaf
-        //sadfasdfkekke
-        //dsafsa
-        //sdfsadf
-//1234567890-
     }
 }
