70487d620fa17d2efbf453fa39c7925056920726
semh <sem.huits@hva.nl>
2020-02-26 12:52:30 +0100

    kaas

--- Automata programming/src/com/company/Main.java
+++ Automata programming/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //kaas is lekkerhg
+        // write your code here
+        //12345
     }
 }
